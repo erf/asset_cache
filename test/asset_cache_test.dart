a406fda7ad62ade11e73e2feb383e34e0ae8ff36
Erlend Lind Madsen <erlendf80@gmail.com>
2022-04-21 00:35:02 +0200

    test: add group for MyCache

--- test/asset_cache_test.dart
+++ test/asset_cache_test.dart
@@ -30,47 +30,49 @@ class MyCache extends GenericCache<String> {
 }
 
 void main() {
-  test('cache and get same value back', () async {
-    final cache = MyCache();
-    final result = await cache.load("test");
-    expect(result, "test");
-  });
+  group("Custom GenericCache MyCache tests", () {
+    test('cache and get same value back', () async {
+      final cache = MyCache();
+      final result = await cache.load("test");
+      expect(result, "test");
+    });
 
-  test('cache and get same value back twice', () async {
-    final cache = MyCache();
-    final result1 = await cache.load("test");
-    final result2 = await cache.load("test");
-    expect(result1, "test");
-    expect(result2, "test");
-  });
+    test('cache and get same value back twice', () async {
+      final cache = MyCache();
+      final result1 = await cache.load("test");
+      final result2 = await cache.load("test");
+      expect(result1, "test");
+      expect(result2, "test");
+    });
 
-  test('cache and get two different values', () async {
-    final cache = MyCache();
-    final result1 = await cache.load("test1");
-    final result2 = await cache.load("test2");
-    expect(result1, "test1");
-    expect(result2, "test2");
-  });
+    test('cache and get two different values', () async {
+      final cache = MyCache();
+      final result1 = await cache.load("test1");
+      final result2 = await cache.load("test2");
+      expect(result1, "test1");
+      expect(result2, "test2");
+    });
 
-  test('only cache once', () async {
-    final cache = MyCache();
-    final result1 = await cache.load("test");
-    final result2 = await cache.load("test");
-    final result3 = await cache.load("test");
-    expect(result1, "test");
-    expect(result2, "test");
-    expect(result3, "test");
-    expect(cache.counter, 1);
-  });
+    test('only cache once', () async {
+      final cache = MyCache();
+      final result1 = await cache.load("test");
+      final result2 = await cache.load("test");
+      final result3 = await cache.load("test");
+      expect(result1, "test");
+      expect(result2, "test");
+      expect(result3, "test");
+      expect(cache.counter, 1);
+    });
 
-  test('only cache twice', () async {
-    final cache = MyCache();
-    final result1 = await cache.load("test");
-    final result2 = await cache.load("HELLO");
-    final result3 = await cache.load("test");
-    expect(result1, "test");
-    expect(result2, "HELLO");
-    expect(result3, "test");
-    expect(cache.counter, 2);
+    test('only cache twice', () async {
+      final cache = MyCache();
+      final result1 = await cache.load("test");
+      final result2 = await cache.load("HELLO");
+      final result3 = await cache.load("test");
+      expect(result1, "test");
+      expect(result2, "HELLO");
+      expect(result3, "test");
+      expect(cache.counter, 2);
+    });
   });
 }
